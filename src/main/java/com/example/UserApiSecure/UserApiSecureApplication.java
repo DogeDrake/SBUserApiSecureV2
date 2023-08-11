94893ff3c0d005d95e6d530940fe2d8fe3c40f27
Rodriguez, Luis I <luis.i.rodriguez@accenture.com>
2023-08-11 20:19:05 +0200

    Repository Init

---
+++ src/main/java/com/example/UserApiSecure/UserApiSecureApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserApiSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserApiSecure/UserApiSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserApiSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
